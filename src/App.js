885f49202db50859b0ef250943ec1f902dff635d
Derek Chen <derekgc@icloud.com>
2018-07-16 17:56:40 -0400

    add some svg

--- src/App.js
+++ src/App.js
@@ -2,20 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <svg height="200" width="300" style={{border: "1px solid #16a085"}} viewBox="0 0 100 100">
+          <path d="M 20 20 C 20 110, 110 110, 110 20" stroke="green" fill="transparent"/>
+
+          <path d="M 20 20 V 80 H 50" stroke="black" stroke-width="2" fill="transparent" />
+        </svg>
+
+        <svg>
+        <circle cx={100} cy={100} r={50} />
+        </svg>
+
       </div>
     );
   }
+
+  setSearch(data) {
+    const action = {
+      type: "LOAD_EVENT", // variety of things
+      payload: {
+        data: data,
+        // value: event.target.value,
+      } // bunch of stuff being delivered
+    }
+    this.props.dispatch(action);
+  }
+
+
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    // shows: state.shows,
+    // selectedShow: state.selectedShow,
+    // searchTerm: state.searchTerm
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+    dispatch
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+
--- src/index.js
+++ src/index.js
@@ -4,5 +4,61 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+    shows: [],
+    selectedShow: null,
+    searchTerm: "",
+}
+
+
+const reducer = (state = initialState, action, id) => {
+    console.log('state', state);
+    console.log('action', action);
+    console.log('id:arg', id);
+    console.log('id:meow', action.id);
+
+    switch(action.type) {
+      case "CLICK_EVENT":
+        const { data } = action.payload;
+        let found = state.shows.find( e => e.id === data)
+        return {...state, selectedShow: found }
+
+      case "LOAD_EVENT":
+        return { ...state, shows: action.payload.data }
+      case "SEARCH_EVENT":
+        return { ...state, searchTerm: action.payload.data }
+    //   case "TOGGLE_DANCING":
+    //     return { ...state, dancing: !state.dancing }
+    //   case "SET_COUNTER":
+    //     return { ...state, counter: action.payload.value }
+      default:
+        return state;
+    }
+  
+  }
+
+const store = createStore(reducer);
+
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    console.group("SPECIAL")
+    console.log('%c before', 'color: blue', store.getState());
+    const returnValue = rawDispatch(action);
+    console.log('%c after', 'color: red', store.getState());
+    console.groupEnd()
+    return returnValue;
+  }
+}
+
+store.dispatch = addLoggingToDispatch(store);
+
+console.log(store);
+console.log('after createStore', store.getState());
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
