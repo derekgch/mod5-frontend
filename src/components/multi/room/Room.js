eb743bad7c89dd8657a83871d331f238d172d8dd
[Derek] <[derekgc@icloud.com]>
2018-10-03 15:53:29 -0400

    add socket to room

--- src/components/multi/room/Room.js
+++ src/components/multi/room/Room.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import ChatContainer from './ChatContainer';
 import ListContainer from './ListContainer';
+import openSocket from 'socket.io-client';
+import Adapter, { ip } from '../../../Adapter';
+
 
 class Room extends Component {
+    constructor(props){
+        super(props);
+        this.socket = openSocket(`http://${ip}:5000`);
+        this.state={
+            self:null,
+            message:"",
+            others:[]
+        }
+
+    }
+
+    componentDidMount(){
+        this.socket.emit("RESTART");
+        this.socket.on("ALL_PLAYERS", data => this.getPlayers(data));
+        console.log("mount")
+    }
+
+    getPlayers=(data)=>{
+        console.log("getplayers")
+        Object.keys(data).forEach( (id)  => {
+            if (data[id].playerId === this.socket.id) {
+                console.log("self", id);
+                this.setState({self:data[id]})
+            } else {
+                console.log("others",id);
+                this.setState({others:[...this.state.others, data[id]]})
+            }
+        })
+    }
+
+
+
     render() {
         return (
             <div className="room-container">
                 <ListContainer />
 
-                <ChatContainer />
+                <ChatContainer socket={this.socket}/>
 
                 
             </div>
