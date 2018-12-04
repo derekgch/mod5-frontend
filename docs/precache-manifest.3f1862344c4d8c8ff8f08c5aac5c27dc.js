48b881764d1a9217a49b82ab984bda2f002d6295
Derek Chen <derekgc@icloud.com>
2018-12-03 19:41:58 -0500

    single player built

---
+++ docs/precache-manifest.3f1862344c4d8c8ff8f08c5aac5c27dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c66b9051294f527b7ed718977ad18b97",
+    "url": "/play-and-learn-frontend/static/media/sd.c66b9051.png"
+  },
+  {
+    "revision": "3783a97e6953be89955d374dffdc9144",
+    "url": "/play-and-learn-frontend/static/media/keyboard.3783a97e.png"
+  },
+  {
+    "revision": "cda3fc295ccf4e0794ce",
+    "url": "/play-and-learn-frontend/static/js/runtime~main.cda3fc29.js"
+  },
+  {
+    "revision": "12ae0fc9c79a2af32f64",
+    "url": "/play-and-learn-frontend/static/js/main.12ae0fc9.chunk.js"
+  },
+  {
+    "revision": "4bbc6b62be5bdcdc93e6",
+    "url": "/play-and-learn-frontend/static/js/1.4bbc6b62.chunk.js"
+  },
+  {
+    "revision": "12ae0fc9c79a2af32f64",
+    "url": "/play-and-learn-frontend/static/css/main.eff19328.chunk.css"
+  },
+  {
+    "revision": "f2686b91362eb1f4eb4c89a97f49fd56",
+    "url": "/play-and-learn-frontend/index.html"
+  }
+];
